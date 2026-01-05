1a02d283ca67d1255b21204c5667b84c80b1f79b
Reuben Brewer <reuben.brewer@gmail.com>
2026-01-04 16:33:56 -0800

    Add files via upload

--- CopyAll_PythonFiles_IngeniaBLDC_ReubenPython3Class.py
+++ CopyAll_PythonFiles_IngeniaBLDC_ReubenPython3Class.py
@@ -6,12 +6,14 @@ reuben.brewer@gmail.com
 www.reubotics.com
 
 Apache 2 License
-Software Revision L, 06/16/2025
+Software Revision N, 12/26/2025
 
-Verified working on: Python 3.11/3.12 for Windows 10, 11 64-bit.
+Python 3.11/12 but NOT 3.13 (ingenialink requires scipy==1.12.0 compatible, which is NOT compatible with Python 3.13)
 '''
 
 __author__ = 'reuben.brewer'
+
+#######################################################################################################################
 
 ########################################
 import datetime
@@ -26,6 +28,8 @@ import traceback
 import distutils #For CopyEntireDirectoryWithContents(). Both imports are needed to avoid errors in 'distutils.dir_util.copy_tree("./foo", "./bar")'
 from distutils import dir_util #For CopyEntireDirectoryWithContents(). Both imports are needed to avoid errors in 'distutils.dir_util.copy_tree("./foo", "./bar")'
 ########################################
+
+#######################################################################################################################
 
 #######################################################################################################################
 def CreateNewDirectory(directory):
@@ -63,8 +67,8 @@ def IsTheTimeCurrentlyAM():
         return 0
 #######################################################################################################################
 
-FileWorkingDirectory = "D:\\" #Use "E:\\" if the filepath length is too long
-#FileWorkingDirectory = os.getcwd() #Use "E:\\" if the filepath length is too long
+FileWorkingDirectory = "D:\\" #Use "D:\\" if the filepath length is too long
+#FileWorkingDirectory = os.getcwd() #Use "D:\\" if the filepath length is too long
 
 AMflag = IsTheTimeCurrentlyAM()
 if AMflag == 1:
@@ -80,13 +84,11 @@ try:
     ###
     CopyEntireDirectoryWithContents("G:\\My Drive\\CodeReuben\\IngeniaBLDC_ReubenPython3Class\\InstallFiles_and_SupportDocuments", FileDirectoryToCreate + "\\InstallFiles_and_SupportDocuments")  # Copies the entire directory
 
+    shutil.copy("G:\\My Drive\\CodeReuben\\ReubenGithubCodeModulePaths\\ReubenGithubCodeModulePaths.py", FileDirectoryToCreate)
+
+    shutil.copy("G:\\My Drive\\CodeReuben\\IngeniaBLDC_ReubenPython3Class\\__init__.py", FileDirectoryToCreate)
     shutil.copy("G:\\My Drive\\CodeReuben\\IngeniaBLDC_ReubenPython3Class\\IngeniaBLDC_ReubenPython3Class.py", FileDirectoryToCreate)
     shutil.copy("G:\\My Drive\\CodeReuben\\IngeniaBLDC_ReubenPython3Class\\test_program_for_IngeniaBLDC_ReubenPython3Class.py", FileDirectoryToCreate)
-
-    shutil.copy("G:\\My Drive\\CodeReuben\\IngeniaBLDC_ReubenPython3Class\\ResetWinPCAPdriver.py", FileDirectoryToCreate)
-    shutil.copy("G:\\My Drive\\CodeReuben\\IngeniaBLDC_ReubenPython3Class\\ResetWinPCAPdriver.bat", FileDirectoryToCreate)
-
-    shutil.copy("G:\\My Drive\\CodeReuben\\IngeniaBLDC_ReubenPython3Class\\Batch_Admin.bat", FileDirectoryToCreate)
 
     shutil.copy("G:\\My Drive\\CodeReuben\\IngeniaBLDC_ReubenPython3Class\\SimplestTest_SingleSlave_SDOnoPDO_IngeniaBLDC_ReubenPython3Class.py", FileDirectoryToCreate)
     shutil.copy("G:\\My Drive\\CodeReuben\\IngeniaBLDC_ReubenPython3Class\\SimplestTest_SingleSlave_SDOnoPDO_IngeniaBLDC_ReubenPython3Class_NonAdmin.bat", FileDirectoryToCreate)
@@ -108,6 +110,9 @@ try:
 
     shutil.copy("G:\\My Drive\\CodeReuben\\EntryListWithBlinking_ReubenPython2and3Class\\EntryListWithBlinking_ReubenPython2and3Class.py", FileDirectoryToCreate)
     #shutil.copy("G:\\My Drive\\CodeReuben\\EntryListWithBlinking_ReubenPython2and3Class\\test_program_for_EntryListWithBlinking_ReubenPython2and3Class.py", FileDirectoryToCreate)
+
+    shutil.copy("G:\\My Drive\\CodeReuben\\GetCPUandMemoryUsageOfProcessByPID_ReubenPython3Class\\GetCPUandMemoryUsageOfProcessByPID_ReubenPython3Class.py", FileDirectoryToCreate)
+    #shutil.copy("G:\\My Drive\\CodeReuben\\GetCPUandMemoryUsageOfProcessByPID_ReubenPython3Class\\test_program_for_GetCPUandMemoryUsageOfProcessByPID_ReubenPython3Class.py", FileDirectoryToCreate)
 
     shutil.copy("G:\\My Drive\\CodeReuben\\LowPassFilter_ReubenPython2and3Class\\LowPassFilter_ReubenPython2and3Class.py", FileDirectoryToCreate)
     #shutil.copy("G:\\My Drive\\CodeReuben\\LowPassFilter_ReubenPython2and3Class\\test_program_for_LowPassFilter_ReubenPython2and3Class.py", FileDirectoryToCreate)
